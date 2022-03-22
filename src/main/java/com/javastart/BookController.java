557c43a6575ec6c4147ffb8c54d25b6c06caea80
Krzysztof <krzubrycki@gmail.com>
2022-03-22 13:50:06 +0100

    reduce repeated code with annotations

--- src/main/java/com/javastart/BookController.java
+++ src/main/java/com/javastart/BookController.java
@@ -2,15 +2,17 @@ package com.javastart;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
 
-    @GetMapping("/api/books")
-    @ResponseBody
+    @GetMapping
     List<Book> getAllBooks() {
         return List.of(
                 new Book("Harry Potter", "J.K. Rowling", 1997, 223),
@@ -19,8 +21,7 @@ public class BookController {
         );
     }
 
-    @GetMapping("/api/books/1")
-    @ResponseBody
+    @GetMapping("/1")
     Book getSingleBook(){
         return new Book("Harry Potter", "J.K. Rowling", 1997, 223);
     }
